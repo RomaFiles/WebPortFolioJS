6bf7e89da0dc5a9ff65beb6b7144bd198e59d1ae
Rodrigo Magaña <roma.mb89@gmail.com>
2024-09-01 04:32:58 -0600

    Portfolio Website Preview 1 (navigation bar)

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.getElementById("work-btn").addEventListener("click", function() {
+    this.classList.add("active");
+    this.classList.remove("inactive");
+    document.getElementById("play-btn").classList.remove("active");
+    document.getElementById("play-btn").classList.add("inactive");
+});
+
+document.getElementById("play-btn").addEventListener("click", function() {
+    this.classList.add("active");
+    this.classList.remove("inactive");
+    document.getElementById("work-btn").classList.remove("active");
+    document.getElementById("work-btn").classList.add("inactive");
+});
+
+document.querySelector('.menu-button').addEventListener('click', function() {
+    this.classList.toggle('active'); // Alterna la clase active para mostrar la "X"
+});
